e41dd046a7f13b1fabdb023aa4c7fa4b2a92cffd
Anjasfedo <fedoaf98@gmail.com>
2024-02-10 16:35:39 +0700

    Server

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup>
 const { sayHello } = useUtils();
 sayHello();
+
+const { data } = await useFetch('/api/hello')
 </script>
 
 <template>
+  {{ data }}
   <Counter />
 </template>
 
