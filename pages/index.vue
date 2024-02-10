18b89988e6e45cfd4649fcdc03acb7d297ccd45a
Anjasfedo <fedoaf98@gmail.com>
2024-02-10 10:58:48 +0700

    Assets

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,11 @@
 
 <template>
   <div>Page: index</div>
+  <img src="@/assets/img-blank.png" alt="">
+  <img src="/img-blank.png" alt="">
+  <span class="p-24 text-red-300 pt-20">
+    <MemoryAlert />
+  </span>
 </template>
 
 <style scoped></style>
